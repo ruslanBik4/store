50e580561f5d566628bb13b4e9802ab34c59538b
ruslan-bik <ruslan-bik@yandex.ru>
2016-09-08 13:17:40 +0300

    fromId

--- web/js/validate.js
+++ web/js/validate.js
@@ -1,10 +1,29 @@
 function Validate(thisForm) {
 
-    if (thisForm.id = 'adminLogin') {
-        result = ($('input[name=login]').val() > '') && ($('input[name=password]').val() > '');
-        if (!result) {
-            alert('Неверные данные');
+    if (thisForm.id == 'adminLogin') {
+        result = true;
+        $('input[type!=submit]:visible', thisForm).each(
+            function (index) {
+                if ( !this.value ) {
+                    result = false;
+                    alert( 'Заполните поле - ' + this.name);
+                    $(this).css( { border: '1px red solid' } ).focus();
+
+                    return false;
+                }
+                else {
+                    $(this).css( { border: '' } );
+                }
+            }
+        );
+
+        if (result && (divPassword.style.display == 'none') )  {
+            divPassword.style.display = 'block';
+            alert('А теперь введите пароль!');
+
+            return false;
         }
+
         return result && (confirm('Хотите ли Вы отправить форму?'));
     }
 }
