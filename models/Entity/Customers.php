96a84e3c98c4224ac7a9cb02ee525d25421d6b7e
ruslan-bik <ruslan-bik@yandex.ru>
2016-09-10 10:54:43 +0300

    Customers

---
+++ models/Entity/Customers.php
@@ -0,0 +1,60 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: rus
+ * Date: 10.09.16
+ * Time: 10:48
+ */
+class Customers
+{
+    const TABLE_NAME = 'Customers';
+    // primary key, autoincrement
+    private $id;
+    /**
+     * @var string
+     * @ORM\Column(name="name", type="string", length=255)
+     * @Assert\NotBlank()
+     */
+    private $name;
+
+    /**
+     * @var string
+     * @ORM\Column(name="email", type="string", length=255)
+     * @Assert\NotBlank()
+     */
+    private $email;
+    /**
+     * @var string
+     * @ORM\Column(name="phone", type="string", length=25)
+     */
+    private $phone;
+    /**
+     * @var timestamp
+     * @ORM\Column(name="date_reg", type="string", length=255)
+     * @Assert\NotBlank()
+     */
+    private $dateReg;
+
+    /**
+     * Определяем число записей в таблице
+     * @return integer
+     */
+    static public function CountRecord()
+    {
+        return 'select count(*) from ' . self::TABLE_NAME;
+
+    }
+    /**
+
+    /**
+     * получение всех записей таблицы
+     * @return bool|mysqli_result
+     */
+    static public function SelectAll()
+    {
+        return "select * from  " . self::TABLE_NAME;
+
+    }
+
+}
